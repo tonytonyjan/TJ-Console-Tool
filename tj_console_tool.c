0ccd78be57464613901b62f59002e8109d189b83
Tony Jian <tonytonyjan@gmail.com>
2012-10-20 00:01:38 +0800

    setCursorPosition

--- tj_console_tool.c
+++ tj_console_tool.c
@@ -4,3 +4,8 @@
 BOOL setTitle(const char* title){
   return SetConsoleTitle((LPCSTR)title);
 }
+
+BOOL setCursorPosition(unsigned short x, unsigned short y){
+  COORD coord = {(SHORT)x * 2, (SHORT)y};
+  return SetConsoleCursorPosition(H_CONSOLE_OUTPUT, coord);
+}
--- tj_console_tool.h
+++ tj_console_tool.h
@@ -3,6 +3,10 @@
 
 #include <windef.h>
 
+#define H_CONSOLE_OUTPUT GetStdHandle(STD_OUTPUT_HANDLE)
+#define H_CONSOLE_INPUT GetStdHandle(STD_INPUT_HANDLE)
+
 BOOL setTitle(const char* title);
+BOOL setCursorPosition(unsigned short x, unsigned short y);
 
 #endif  // TJ_CONSOLE_TOOL_TJ_CONSOLE_TOOL_H_
